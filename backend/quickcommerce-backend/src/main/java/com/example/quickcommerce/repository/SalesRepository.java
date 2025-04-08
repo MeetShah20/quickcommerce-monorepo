e02a4dbf3bacd5e3862e0115ee2beb5fd1b28a3a
MeetShah20 <meetshah3434@gmail.com>
2025-04-08 12:01:25 +0530

    Removed subtree folders to convert to submodules

--- backend/quickcommerce-backend/src/main/java/com/example/quickcommerce/repository/SalesRepository.java
+++
@@ -1,8 +0,0 @@
-
-package com.example.quickcommerce.repository;
-
-import com.example.quickcommerce.model.Sales;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SalesRepository extends JpaRepository<Sales, Long> {
-}
